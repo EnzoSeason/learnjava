32d2591e6cd55e2bfcd6aaffb5985b5f3203e645
Enzo Liu <enzoliu@192.168.0.106>
2017-07-29 18:42:59 +0800

    code of Accum

---
+++ threadtest/Accum.java
@@ -0,0 +1,18 @@
+class Accum{
+    private static Accum a = new Accum();
+    private int counter = 0;
+    
+    private Accum(){}
+    
+    public static Accum getAccum(){
+        return a;
+    }
+    
+    public int getCounter(){
+        return counter;
+    }
+    
+    public void updateCounter(int add){
+        counter += add;
+    }
+}
